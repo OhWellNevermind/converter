b4e003d0ac8063e773a53ebcc4f6125ff74b6518
OhWellNevermind <52574266+OhWellNevermind@users.noreply.github.com>
2023-07-30 11:38:05 +0300

    new function add unit to converter

--- index.js
+++ index.js
@@ -13,17 +13,15 @@ class Converter {
   }
 
   convert() {
-    console.log(this.value, this.from, this.to);
-    console.log(typeof this.to.unit);
     if (this.to.unit == "m") {
       return JSON.stringify({
         unit: this.to.unit,
-        value: (this.value * this.from.toMeter).toFixed(4),
+        value: (this.value * this.from.toMeter).toFixed(2),
       });
     }
     return JSON.stringify({
       unit: this.to.unit,
-      value: ((this.value * this.from.toMeter) / this.to.toMeter).toFixed(4),
+      value: ((this.value * this.from.toMeter) / this.to.toMeter).toFixed(2),
     });
   }
 }
@@ -33,9 +31,29 @@ const refs = {
   convertTo: document.querySelector(".convert-to"),
   input: document.querySelector(".input"),
   output: document.querySelector(".output"),
+  converterContainer: document.querySelector('.converter-container'),
+  form: document.querySelector('.add-form')
 };
+const units = new Units()
 
-refs.input.addEventListener("input", (event) => {
+refs.converterContainer.addEventListener('input', handleInput)
+refs.form.addEventListener('submit', addUnit)
+
+
+function convertTo(jsonConvertData) {
+  const data = JSON.parse(jsonConvertData);
+  if (
+    Object.hasOwn(units, data.convertTo) &&
+    Object.hasOwn(units, data.distance.unit)
+  ) {
+    const from = units[data.distance.unit];
+    const to = units[data.convertTo];
+    const converter = new Converter(data.distance.value, from, to);
+    return converter.convert();
+  }
+}
+
+function handleInput() {
   if (!refs.input.value.trim()) {
     refs.output.value = "";
     return;
@@ -53,19 +71,31 @@ refs.input.addEventListener("input", (event) => {
   });
   const value = JSON.parse(convertTo(jsonData));
   refs.output.value = value.value;
-});
+}
 
-function convertTo(jsonConvertData) {
-  const data = JSON.parse(jsonConvertData);
-  const units = new Units();
+function addUnit(event){
+  event.preventDefault()
 
-  if (
-    Object.hasOwn(units, data.convertTo) &&
-    Object.hasOwn(units, data.distance.unit)
-  ) {
-    const from = units[data.distance.unit];
-    const to = units[data.convertTo];
-    const converter = new Converter(data.distance.value, from, to);
-    return converter.convert();
+  const {
+    elements: { unit,unitFull, toMeters }
+  } = event.currentTarget;
+
+  const unitValue = unit.value.trim()
+  const unitFullValue = unitFull.value.trim()
+  const toMetersValue = toMeters.value.trim()
+
+  if(!unitValue || !unitFullValue || !toMetersValue) {
+    alert('Please fill all fields')
+    return
   }
+
+    refs.convertFrom.insertAdjacentHTML('beforeend', `<option value="${unitValue}">${unitFullValue}</option>`)
+    refs.convertTo.insertAdjacentHTML('beforeend', `<option value="${unitValue}">${unitFullValue}</option>`)
+    Object.defineProperty(units, unitValue, {
+      value: {
+    unit: unitValue, toMeter: parseFloat(toMetersValue)
+    },
+    writable:false
+  })
+
 }
